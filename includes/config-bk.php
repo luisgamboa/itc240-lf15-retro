bc8a0353ecc76c44ab0dd341f90bba582adbdd0b
Luis Gamboa <luisgamboasierra@gmail.com>
2015-11-23 17:54:19 -0800

    Adds Pager Class code to repo
    Needs to be integrated

--- includes/config-bk.php
+++ includes/config-bk.php
@@ -1,6 +1,24 @@
 <?php
 //config.php
 
+define('DEBUG',TRUE); #we want to see all errors
+
+date_default_timezone_set('America/Los_Angeles'); #sets default date/timezone for this website
+
+include 'common.php'; //stores all unsightly application functions, etc.
+include 'credentials.php;'//credentials
+include 'MyAutoLoader.php'; //loads class that autoloads all classes in include folder
+
+/* automatic path settings - use the following path settings for placing all code in one application folder */ 
+define('VIRTUAL_PATH', 'http://lgamboa.com/retro/'); # Virtual (web) 'root' of application for images, JS & CSS files
+define('PHYSICAL_PATH', '/home/luigam1/lgamboa.com/retro/'); # Physical (PHP) 'root' of application for file & upload reference
+define('INCLUDE_PATH', PHYSICAL_PATH . 'includes/'); # Path to PHP include files - INSIDE APPLICATION ROOT
+
+ob_start();  #buffers our page to be prevent header errors. Call before INC files or ANY html!
+header("Cache-Control: no-cache");header("Expires: -1");#Helps stop browser & proxy caching
+
+
+
 //THIS DEFINES THE CURRENT FILE NAME
 
 define('THIS_PAGE',basename( $_SERVER['PHP_SELF']) );//it will create a constant, are simple data
@@ -112,7 +130,50 @@ function makeLinks($arr,$prefix='',$suffix='',$exception='')
     return $myReturn;
 }//END OF MAKELINKS() FUNCTION
 
+//new code
+/**
+ * generic error handling function for hiding db errors, etc.
+ *
+ * Change the socks reference and any HTML to suit
+ *
+ * @param string $myFile File from which error emitted
+ * @param string $myLine Line where error can be found
+ * @param string $errorMsg Message to be shown to admin only
+ * @return void
+ * @see dbIn()
+ * @todo none
+ */
 
+function myerror($myFile, $myLine, $errorMsg)
+{
+    if(defined('DEBUG') && DEBUG)
+    {
+       echo "Error in file: <b>" . $myFile . "</b> on line: <b>" . $myLine . "</b><br />";
+       echo "Error Message: <b>" . $errorMsg . "</b><br />";
+       die();
+    }else{
+        echo "I'm sorry, we have encountered an error.  Would you like to buy some socks?";
+        die();
+    }
+}
+
+/**
+ * Wrapper function for processing data pulled from db
+ *
+ * Forward slashes are added to MySQL data upon entry to prevent SQL errors.  
+ * Using our dbOut() function allows us to encapsulate the most common functions for removing  
+ * slashes with the PHP stripslashes() function, plus the trim() function to remove spaces.
+ *
+ * @param string $str data as pulled from MySQL
+ * @return $str data cleaned of slashes, spaces around string, etc.
+ * @see dbIn()
+ * @todo none
+ */
+function dbOut($str)
+{
+	if($str!=""){$str = stripslashes(trim($str));}//strip out slashes entered for SQL safety
+	return $str;
+} #End dbOut()
 
 
 
